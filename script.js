f7e146b88e580c9e2d127956a4dcc6d97093b097
sunyang9876 <sunyang9876@hotmail.com>
2019-11-07 20:58:49 +1000

    initial

---
+++ script.js
@@ -0,0 +1,60 @@
+// css class for different card image
+const CARD_TECHS = [
+  'html5',
+  'css3',
+  'js',
+  'sass',
+  'nodejs',
+  'react',
+  'linkedin',
+  'heroku',
+  'github',
+  'aws'
+];
+
+// only list out some of the properties,
+// add more when needed
+const game = {
+  score: 0,
+  level: 1,
+  timer: 60,
+  timerDisplay: null,
+  scoreDisplay: null,
+  levelDisplay: null,
+  timerInterval: null,
+  startButton: null
+  // and much more
+};
+
+setGame();
+
+/*******************************************
+/     game process
+/******************************************/
+function setGame() {
+  // register any element in your game object
+}
+
+function startGame() {}
+
+function handleCardFlip() {}
+
+function nextLevel() {}
+
+function handleGameOver() {}
+
+/*******************************************
+/     UI update
+/******************************************/
+function updateScore() {}
+
+function updateTimerDisplay() {}
+
+/*******************************************
+/     bindings
+/******************************************/
+function bindStartButton() {}
+
+function unBindCardClick(card) {}
+
+function bindCardClick() {}
